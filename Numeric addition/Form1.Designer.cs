7ed7b4fc9ed0967747c0655ca9b74c33f9fddc91
Daniel Seavers <daniel.p.seavers@gmail.com>
2020-04-27 13:39:23 +0100

    Developed program

--- Numeric addition/Form1.Designer.cs
+++ Numeric addition/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BTN_Sum = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBX_Input = new System.Windows.Forms.TextBox();
+            this.TBX_Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BTN_Sum
+            // 
+            this.BTN_Sum.Location = new System.Drawing.Point(12, 118);
+            this.BTN_Sum.Name = "BTN_Sum";
+            this.BTN_Sum.Size = new System.Drawing.Size(318, 38);
+            this.BTN_Sum.TabIndex = 0;
+            this.BTN_Sum.Text = "Find the sum of the digits";
+            this.BTN_Sum.UseVisualStyleBackColor = true;
+            this.BTN_Sum.Click += new System.EventHandler(this.BTN_Sum_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sum of digits calculator";
+            // 
+            // TBX_Input
+            // 
+            this.TBX_Input.Location = new System.Drawing.Point(12, 83);
+            this.TBX_Input.Name = "TBX_Input";
+            this.TBX_Input.Size = new System.Drawing.Size(318, 20);
+            this.TBX_Input.TabIndex = 2;
+            this.TBX_Input.Text = "Number Input";
+            // 
+            // TBX_Output
+            // 
+            this.TBX_Output.Location = new System.Drawing.Point(12, 174);
+            this.TBX_Output.Name = "TBX_Output";
+            this.TBX_Output.Size = new System.Drawing.Size(318, 20);
+            this.TBX_Output.TabIndex = 3;
+            this.TBX_Output.Text = "Number Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(344, 243);
+            this.Controls.Add(this.TBX_Output);
+            this.Controls.Add(this.TBX_Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTN_Sum);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BTN_Sum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBX_Input;
+        private System.Windows.Forms.TextBox TBX_Output;
     }
 }
 
--- Numeric addition/Form1.cs
+++ Numeric addition/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Numerics;
 namespace Numeric_addition
 {
     public partial class Form1 : Form
@@ -15,6 +15,23 @@ namespace Numeric_addition
         public Form1()
         {
             InitializeComponent();
+        }
+        private void BTN_Sum_Click(object sender, EventArgs e)
+        {
+            if (BigInteger.TryParse(TBX_Input.Text, out BigInteger i))
+            {
+                char[] ints = TBX_Input.Text.ToCharArray();
+                int output = 0;
+                foreach (char j in ints)
+                {
+                    output += int.Parse(j.ToString());
+                }
+                TBX_Output.Text = output.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter an integer.");
+            }
         }
     }
 }
